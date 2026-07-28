6fd17008ad8aec01f4c2945e7b11085d191323a6
yemi <o.ogunbadejo7@gmail.com>
2026-07-28 12:29:08 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  return <div className="text-3xl font-bold ">App</div>
+}
+
+export default App
