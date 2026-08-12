8f0d4122f1b8b63b354db189f77b907da6205025
yemi <o.ogunbadejo7@gmail.com>
2026-08-12 15:09:04 +0100

    fix: reducing amount of stars

--- src/components/StarsCanvas.jsx
+++ src/components/StarsCanvas.jsx
@@ -8,7 +8,7 @@ import MyLoader from "./MyLoader"
 function Stars(props) {
     const ref = useRef()
 
-    const sphere = random.inSphere(new Float32Array(5000 * 3), { radius: 1.2 })
+    const sphere = random.inSphere(new Float32Array(2000 * 3), { radius: 1.2 })
 
     useFrame((state, delta) => {
         ref.current.rotation.x -= delta / 10
